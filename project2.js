f8a5b3eba8dccb8b385536bbe9ff810a6bfe33dc
PelinalWS <batucan971@gmail.com>
2023-10-29 17:44:55 +0300

    Added functions multi3x3 and index_matrix

--- project2.js
+++ project2.js
@@ -18,13 +18,15 @@ function GetTransform(positionX, positionY, rotation, scale) {
     //     | 1  0  tx |           | Sx * cos(rad) -Sx * sin(rad)  0 |                | Sx * cos(rad) -Sx * sin(rad)  tx |
     // T = | 0  1  ty |   S x R = | Sy * sin(rad)  Sy * cos(rad)  0 |    T x S x R = | Sy * sin(rad)  Sy * cos(rad)  ty |
     //     | 0  0  1  |           |      0              0         1 |                |      0              0         1  |
+    // the drone starts at the point (0, 0)
     const m = [
                  scale * cos, scale * sin, positionX,
                 -scale * sin, scale * cos, positionY,
                        0    ,       0    ,     1    
             ];
-
-    const matrix = [m[0], m[3], m[6], m[1], m[4], m[7], m[2], m[5], m[8]];
+    // the matrix is currently indexed on the rows and not the columns, making it "Horizontal"
+    // to keep track of why the index_matrix is being used, we put in how we want the matrix to be as a string
+    const matrix = index_matrix(m , "Vertical")
     return matrix;
 }
 
@@ -32,26 +34,40 @@ function GetTransform(positionX, positionY, rotation, scale) {
 // The inputs consist of transformation matrices following the identical format.
 // The resulting transformation initially employs trans1 and subsequently applies trans2.
 function ApplyTransform(trans1, trans2) {
-    const m1 = [trans1[0], trans1[3], trans1[6], trans1[1], trans1[4], trans1[7], trans1[2], trans1[5], trans1[8]];
-    const m2 = [trans2[0], trans2[3], trans2[6], trans2[1], trans2[4], trans2[7], trans2[2], trans2[5], trans2[8]];
-    //if trans1 is applied first, it means trans2 x trans1:
+    const t1 = index_matrix(trans1, "Horizontal");
+    const t2 = index_matrix(trans2, "Horizontal");
+    const matrix = multi3x3(t1, t2);
+    const final_matrix = index_matrix(matrix, "Vertical");
+    return final_matrix;
+}
+
+function multi3x3(t1, t2) {
+    // matrix multiplication
     //     | a b c |       | j k l |            | (aj + bm + cp) (ak + bn + cq) (al + bo + cr) |
     // A = | d e f |   B = | m n o |    A x B = | (dj + em + fp) (dk + en + fq) (dl + eo + fr) |
     //     | g h i |       | p q r |            | (gj + hm + ip) (gk + hn + iq) (gl + ho + ir) |
-    //AxB is so that the A's row values are multiplied by B's column values and summed
-    const t0 = (m2[0] * m1[0]) + (m2[1] * m1[3]) + (m2[2] * m1[6]); //
-    const t1 = (m2[0] * m1[1]) + (m2[1] * m1[4]) + (m2[2] * m1[7]); //
-    const t2 = (m2[0] * m1[2]) + (m2[1] * m1[5]) + (m2[2] * m1[8]); //
-
-    const t3 = (m2[3] * m1[0]) + (m2[4] * m1[3]) + (m2[5] * m1[6]); //
-    const t4 = (m2[3] * m1[1]) + (m2[4] * m1[4]) + (m2[5] * m1[7]); //
-    const t5 = (m2[3] * m1[2]) + (m2[4] * m1[5]) + (m2[5] * m1[8]); //
-
-    const t6 = (m2[6] * m1[0]) + (m2[7] * m1[3]) + (m2[8] * m1[6]); //
-    const t7 = (m2[6] * m1[1]) + (m2[7] * m1[4]) + (m2[8] * m1[7]); //
-    const t8 = (m2[6] * m1[2]) + (m2[7] * m1[5]) + (m2[8] * m1[8]); //
-    
-    const augmented_matrix = [t0, t3, t6, t1, t4, t7, t2, t5, t8];
-    return augmented_matrix;
+    // AxB is so that the A's row values are multiplied by B's column values and summed
+    // first row of A with all columns of B
+    const m0 = (t2[0] * t1[0]) + (t2[1] * t1[3]) + (t2[2] * t1[6]);
+    const m1 = (t2[0] * t1[1]) + (t2[1] * t1[4]) + (t2[2] * t1[7]);
+    const m2 = (t2[0] * t1[2]) + (t2[1] * t1[5]) + (t2[2] * t1[8]);
+    // second row of A with all columns of B
+    const m3 = (t2[3] * t1[0]) + (t2[4] * t1[3]) + (t2[5] * t1[6]);
+    const m4 = (t2[3] * t1[1]) + (t2[4] * t1[4]) + (t2[5] * t1[7]);
+    const m5 = (t2[3] * t1[2]) + (t2[4] * t1[5]) + (t2[5] * t1[8]);
+    // third row of A with all columns of B
+    const m6 = (t2[6] * t1[0]) + (t2[7] * t1[3]) + (t2[8] * t1[6]);
+    const m7 = (t2[6] * t1[1]) + (t2[7] * t1[4]) + (t2[8] * t1[7]);
+    const m8 = (t2[6] * t1[2]) + (t2[7] * t1[5]) + (t2[8] * t1[8]);
+    // the values are added into an array with the "Horizontal" ordering
+    const t = [m0, m1, m2, m3, m4, m5, m6, m7, m8];
+    return t;
 }
-
+// does the same thing regardless of what o value is put as a paramenter, it is just used to determine what the matrix gets turned to
+// puts the matrix into the format used by the html or puts it into the format used by the multi3x3 function
+function index_matrix(matrix, o) {
+    const matrix_indexed = [matrix[0], matrix[3], matrix[6],
+                            matrix[1], matrix[4], matrix[7],
+                            matrix[2], matrix[5], matrix[8]];
+    return matrix_indexed;
+}
